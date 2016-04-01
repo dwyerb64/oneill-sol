a450421b00ddd529de31380999e23249107e131b
dwyerb64 <briandwyer@MacBook-Pro.local>
2016-04-01 17:31:58 +0100

    Added gmap3, navbar transistion and some styling bits for other sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,117 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+'use strict';
+
+var SEMICOLON = SEMICOLON || {};
+
+(function($){
+    
+  //               //
+ //  FUNCTIONS    //
+//               //
+SEMICOLON.documentOnReady = {
+    init: function(){
+    	SEMICOLON.functions.navBar();
+    	SEMICOLON.functions.buildGoogleMap();
+    },
+    
+    windowscroll: function() {
+        
+
+    }
+
+};
+
+SEMICOLON.functions = {
+    
+    navBar: function(){
+    	var navBar = $('.js-navbar');
+    	var offsetHeight = navBar.offset();
+        navBar.affix({offset: {top: offsetHeight.top} });
+
+  	},
+
+  	buildGoogleMap: function(){
+        $('#js-google-map').gmap3({
+          center:[53.300698, -6.177590],
+          zoom:16,
+          mapTypeId: "oneill",
+          mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.SATELLITE, google.maps.MapTypeId.ROADMAP]
+          }
+        })
+        .marker([
+          {position:[53.300698, -6.177590], icon: "http://maps.google.com/mapfiles/marker_grey.png"}
+        ])
+        .styledmaptype(
+        "oneill",
+        [
+          {
+            featureType: 'landscape.man_made',
+            elementType: 'geometry',
+            stylers: [
+            { hue: '#009999' },
+            { saturation: -16 },
+            { lightness: 0 },
+            { visibility: 'simplified' }
+            ]
+          },{
+            featureType: 'poi',
+            elementType: 'all',
+            stylers: [
+            { hue: '#009999' },
+            { saturation: -47 },
+            { lightness: 49 },
+            { visibility: 'off' }
+            ]
+          },
+          {
+              "featureType": "road",
+              "elementType": "geometry.fill",
+              "stylers": [
+                  {
+                      "visibility": "simplified"
+                  },
+                  {
+                      "hue": "#c4b88e"
+                  }
+              ]
+          },
+          {
+              "featureType": "water",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "color": "#527191"
+                  },
+                  {
+                      "visibility": "on"
+                  }
+              ]
+          }
+          ],
+        {name: "O'Neill"}
+      );;
+    
+  }
+};
+
+
+
+  //               //
+ //  DOM objects  //
+//               //
+var $htmlBody = $('body,html'),
+    $window = $(window),
+    isMobDevice = (/iphone|ipad|Android|webOS|iPod|BlackBerry|Windows Phone|ZuneWP7/gi).test(navigator.appVersion);
+
+
+  //          //
+ //  Events  //
+//          //
+$(document).ready( SEMICOLON.documentOnReady.init );
+
+$(document).scroll( SEMICOLON.documentOnReady.windowscroll );
+        
+        
+})(jQuery);
+
+
